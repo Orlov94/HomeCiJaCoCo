ce01a4b8e57082d4a27287e0a808313a55c08c71
Vladislav <vladislav_orlov_94@mail.ru>
2025-01-24 22:45:42 +0300

    new test 100% jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,15 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxNew() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1562, 1570, 1455, 1786, 1622, 1836, 1930, 2034};
+
+        long expected = 2034;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
